f470ab1c8f13984847774386d87ffd88b4c6f226
Shreya <20BCS209@iiitdmj.ac.in>
2025-05-16 19:55:43 +0530

    Initial commit

---
+++ 1_hello_world/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// package is multiple files , fmt = format package from standard library -> , which prints hello world
+// package main is the entry point of the program
+func main() {
+	fmt.Println("Hello, World!")
+}
